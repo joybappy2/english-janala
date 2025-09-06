3155c7afad59f889a958c842f3a0fba5e6d60210
joybappy2 <fan328547@gmail.com>
2025-09-06 12:18:09 +0600

    lesssons button click effect added

--- script/index.js
+++ script/index.js
@@ -1,29 +1,31 @@
 const loadLessons = () => {
-    const url = fetch('https://openapi.programming-hero.com/api/levels/all')
-    const response = url.then( response => response.json() )
-    const json = response.then( jsonData => {
-        displayLessons(jsonData.data)
-    })
-}
+  const url = fetch("https://openapi.programming-hero.com/api/levels/all");
+  const response = url.then((response) => response.json());
+  const json = response.then((jsonData) => {
+    displayLessons( jsonData.data );
+  });
+};
+
 
 const loadlevelword = ( id ) => {
-  const lessonBtn = document.getElementById( 'lesson-btn' )
-  // lessonBtn.style.backgroundColor = "#422AD5"
-  // lessonBtn.style.color = "white"
-  // console.log("lesson btn clicked")
-    const url = `https://openapi.programming-hero.com/api/level/${ id }`
-    fetch( url )
-    .then(response=> response.json())
-        .then( data => {
-        displaylevelword(data.data)
-    } )
-    
-}
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  fetch(url)
+  .then((response) => response.json())
+    .then( ( data ) => {
+      const lessonBtnClicked = document.getElementById( `lesson-btn-${ id }` )
+      const lessonButtons = document.querySelectorAll( '.lesson-btn' )
+      lessonButtons.forEach( btn => {
+      btn.classList.remove('active')
+  })
+      lessonBtnClicked.classList.add('active')
+    displaylevelword( data.data );
+    });
+};
 
-const displaylevelword = words => {
-  const wordContainer = document.getElementById( 'word-container' )
-  wordContainer.innerHTML = ''
-  if ( words.length == 0 ) {
+const displaylevelword = (words) => {
+  const wordContainer = document.getElementById( "word-container" );
+  wordContainer.innerHTML = "";
+  if (words.length == 0) {
     wordContainer.innerHTML = `
     <div class="text-center col-span-full space-y-4 py-5">
         <img src="./assets/alert-error.png" class="mx-auto" alt="">
@@ -32,16 +34,22 @@ const displaylevelword = words => {
       </div>
 
       </section>
-    `
+    `;
   }
-    words.forEach( word => {
-        const wordCard = document.createElement('div')
-        wordCard.innerHTML = `
+  words.forEach((word) => {
+    const wordCard = document.createElement("div");
+    wordCard.innerHTML = `
         
         <div class=" card bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-3">
-        <h2 class="font-bold text-3xl">${word.word? word.word : "❌শব্দ পাওয়া যায়নি"}</h2>
+        <h2 class="font-bold text-3xl">${
+          word.word ? word.word : "❌শব্দ পাওয়া যায়নি"
+        }</h2>
         <p class="font-bold text-lg">Meaning / Pronunciation</p>
-        <p class="font-bangla font-medium text-lg">"${word.meaning? word.meaning: "❌শব্দের অর্থ পাওয়া যায়নি"} / ${word.pronunciation? word.pronunciation : "❌শব্দের উচ্চারণ পাওয়া যায়নি"}"</p>
+        <p class="font-bangla font-medium text-lg">"${
+          word.meaning ? word.meaning : "❌শব্দের অর্থ পাওয়া যায়নি"
+        } / ${
+      word.pronunciation ? word.pronunciation : "❌শব্দের উচ্চারণ পাওয়া যায়নি"
+    }"</p>
         <div class="flex justify-between">
           <button class=" bg-[#1A91FF20] p-2 rounded-lg hover:bg-[#1A91FF80]">
             <i class="fa-solid fa-info"></i>
@@ -52,29 +60,31 @@ const displaylevelword = words => {
         </div>
       </div>
         
-        `
-        wordContainer.appendChild(wordCard)
-    });
-}
+        `;
+    wordContainer.appendChild(wordCard);
+  });
+};
 
-
-const displayLessons = jsonData => {
-    const lessonsContainer = document.getElementById( 'lessons-container' )
-    lessonsContainer.innerHTML = ''
-    for ( const data of jsonData ) {
-        const btnDiv = document.createElement('div')
-        btnDiv.innerHTML = `
+const displayLessons = (jsonData) => {
+  const lessonsContainer = document.getElementById("lessons-container");
+  lessonsContainer.innerHTML = "";
+  for (const data of jsonData) {
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `
         
-        <button id="lesson-btn" onclick="loadlevelword(${data.level_no})" class="btn btn-outline btn-primary">
+        <button id="lesson-btn-${data.level_no}" onclick="loadlevelword(${data.level_no})" class="btn btn-outline btn-primary lesson-btn">
         <span> <i class="fa-solid fa-book-open"></i> </span>
         Lesson- ${data.level_no}
         </button>
         
-        `
-        lessonsContainer.appendChild(btnDiv)
-    }
-} 
-loadLessons()
+        `;
+    lessonsContainer.appendChild(btnDiv);
+  }
+};
+loadLessons();
+
+
+
 
 
 // {
@@ -82,7 +92,6 @@ loadLessons()
 //     "level_no": 2,
 //     "lessonName": "Everyday Words"
 // }
-
 
 // {
 //     "id": 5,
